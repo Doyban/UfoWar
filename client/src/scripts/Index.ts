97a6c3106588167dbb1c539ad836f247705a3a29
Daniel Danielecki <daniel.danielecki@mail.com>
2021-07-29 14:46:26 +0200

    Add: little bit of docs to Index.ts

--- client/src/scripts/Index.ts
+++ client/src/scripts/Index.ts
@@ -3,21 +3,26 @@ import GamePlayScene from "./scenes/gameplay/GamePlayScene";
 
 /**
  * @class Game
- * @description Create the game canvas with respective options that are given
+ * @description Creates the game canvas with respective options that are given.
+ * @extends Phaser.Game
  */
 class Game extends Phaser.Game {
+  /**
+   * @constructor
+   * @description Create a new instance of this class.
+   */
   constructor() {
     const game_config: Phaser.Types.Core.GameConfig = {
       type: Phaser.WEBGL, // Use Renderer WebGL.
       width: 1280,
       height: 840,
       parent: "content", // Use custom div element as parent.
-      scene: [GamePlayScene], // the scenes.
+      scene: [GamePlayScene], // The scene.
       // Add ScaleManager for responsiveness.
       scale: {
         mode: Phaser.Scale.ScaleModes.FIT,
       },
-      // Add matter physics.
+      // Add arcade physics.
       physics: {
         default: "arcade",
       },
@@ -29,6 +34,6 @@ class Game extends Phaser.Game {
 
 // Create the game object once window loaded.
 window.onload = () => {
-  window.focus(); // focus the game tab right after loading
+  window.focus(); // Focus the game tab right after loading.
   new Game();
 };
