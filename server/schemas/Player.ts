9ebc62cb43ff9b7c687104ae473c662cd0e9faa4
Daniel Danielecki <daniel.danielecki@mail.com>
2021-08-05 09:40:05 +0200

    Refactor: alphabetic order Player.ts

--- server/schemas/Player.ts
+++ server/schemas/Player.ts
@@ -2,30 +2,30 @@ import { Schema, ArraySchema, type } from "@colyseus/schema";
 import Bullet from "./Bullet";
 
 export default class Player extends Schema {
+  @type([Bullet])
+  bullets: ArraySchema<Bullet>;
+
+  @type("number")
+  gravity: number;
+
+  @type("number")
+  height: number;
+
   @type("string")
   id: number;
+
+  @type("number")
+  lastProcessedInput: number;
+
+  @type("number")
+  rotation: number;
+
+  @type("number")
+  width: number;
 
   @type("number")
   x: number;
 
   @type("number")
   y: number;
-
-  @type("number")
-  width: number;
-
-  @type("number")
-  height: number;
-
-  @type("number")
-  gravity: number;
-
-  @type("number")
-  rotation: number;
-
-  @type("number")
-  lastProcessedInput: number;
-
-  @type([Bullet])
-  bullets: ArraySchema<Bullet>;
 }
