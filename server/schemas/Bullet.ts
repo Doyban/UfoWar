7d27f7da806f4a887302a9110d660e925c3fc893
Daniel Danielecki <daniel.danielecki@mail.com>
2021-08-05 09:50:40 +0200

    Add: JSDoc for Bullet.ts

--- server/schemas/Bullet.ts
+++ server/schemas/Bullet.ts
@@ -1,5 +1,10 @@
 import { type, Schema } from "@colyseus/schema";
 
+/**
+ * @class Bullet
+ * @description Creates the schema for Bullet.
+ * @extends Schema
+ */
 export default class Bullet extends Schema {
   @type("number")
   angle: number;
